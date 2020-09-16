b9e310ca7cc942049cd7d7eb89ea6e5fdf8745c0
Andrew Smith <andy@robotlovesyou.com>
2020-09-16 15:20:42 +0200

    added first pass at iterator over source clusters

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,104 @@
 // pub mod source;
 // pub mod token;
 
+mod source_segmentation {
+    use crate::source_cluster::SourceCluster;
+    use unicode_segmentation::{UnicodeSegmentation, Graphemes};
+
+    /// Container for an iterator over source segments
+    pub struct SourceSegments<'a> {
+        column: u32,
+        line: u32,
+        graphemes: Graphemes<'a>,
+    }
+
+    impl SourceSegments <'_> {
+        /// construct a new SourceSegments
+        pub fn new(source: &str) -> SourceSegments {
+            SourceSegments {
+                graphemes: source.graphemes(true),
+                column: 1,
+                line: 1,
+            }
+        }
+    }
+
+    impl Iterator for SourceSegments <'_> {
+        type Item = SourceCluster;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            if let Some(cluster) = self.graphemes.next() {
+                // normalise \r\n to \n
+                let normalise_newline = |gc| {
+                    if gc == "\r\n" {"\n"} else {gc}
+                };
+
+                let result = SourceCluster::new(normalise_newline(cluster).to_string(), self.line, self.column);
+
+                if result.is_newline() {
+                    self.line += 1;
+                    self.column = 1;
+                } else {
+                    self.column +=1;
+                }
+
+                Some(result)
+            } else {
+                None
+            }
+        }
+    }
+
+    pub trait SourceSegmentation {
+        ///Return an iterator over source segments
+        fn source_clusters(&self) -> SourceSegments;
+    }
+
+    impl SourceSegmentation for &str {
+        fn source_clusters(&self) -> SourceSegments {
+            SourceSegments::new(self)
+        }
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn source_segments_returns_source_segments_struct() {
+            // if this compiles it works for this test's purposes
+            let _source_seg: SourceSegments = "Mary had a little lamb".source_clusters();
+        }
+
+        #[test]
+        fn source_segments_returns_an_iterator_over_the_source_grapheme_clusters() {
+            let source = "Mary had a little lamb.";
+            assert_eq!(source.source_clusters().count(), source.graphemes(true).count());
+        }
+
+        #[test]
+        fn source_segments_correctly_increments_line_and_column_values() {
+            let source = "ab\r\ncd\r\nநி"; // the \r\n grapheme cluster will be normalised to \n
+            let mut iter = source.source_clusters();
+
+            let test_item = |sc: SourceCluster, el: u32, ec: u32, egc: &str| {
+                assert_eq!(el, sc.line(), "wrong line for cluster {:?}", sc);
+                assert_eq!(ec, sc.column(), "wrong column for cluster {:?}", sc);
+                assert_eq!(sc.grapheme_cluster(), egc, "wrong grapheme cluster for cluster {:?}", sc);
+            };
+
+            test_item(iter.next().expect("no cluster for 'a'"), 1, 1, "a");
+            test_item(iter.next().expect("no cluster for 'b'"), 1, 2, "b");
+            test_item(iter.next().expect("no cluster for '\n'"), 1, 3, "\n");
+            test_item(iter.next().expect("no cluster for 'c'"), 2, 1, "c");
+            test_item(iter.next().expect("no cluster for 'd'"), 2, 2, "d");
+            test_item(iter.next().expect("no cluster for '\n' 2"), 2, 3, "\n");
+            test_item(iter.next().expect("no cluster for 'நி'"), 3, 1, "நி");
+        }
+
+    }
+}
+
 mod source_cluster {
     use std::rc::Rc;
 
@@ -11,67 +109,103 @@ mod source_cluster {
     const LOWERCASE_MASK: u8 = 1 << 3;
     const UPPERCASE_MASK: u8 = 1 << 4;
     const WHITESPACE_MASK: u8 = 1 << 5;
+    const NEWLINE_MASK: u8 = 1 << 6;
 
-    #[derive(Clone)]
+    #[derive(Debug)]
     pub struct SourceCluster {
         grapheme_cluster: Rc<String>,
+        column: u32,
+        line: u32,
         unicode_characteristics: u8,
     }
 
     impl SourceCluster {
-        fn new(source: String) -> SourceCluster {
+        /// Construct a new SourceCluster. The source paramter should be a valid GraphemeCluster as
+        /// returned by the unicode_segmentation crate
+        pub fn new(source: String, line: u32, column: u32) -> SourceCluster {
+            // only test the first char of the grapheme cluster.
+            // uses take(1).fold(...) so that empty clusters have no characteristics
             let unicode_characteristics: u8 = source.chars().take(1).fold(0u8, |mut acc, c|{
-                if c.is_alphabetic() {acc = acc | ALPHA_MASK};
-                if c.is_alphanumeric() {acc = acc | ALPHANUMERIC_MASK};
-                if c.is_digit(10) {acc = acc | DIGIT_MASK};
-                if c.is_lowercase() {acc = acc | LOWERCASE_MASK};
-                if c.is_uppercase() {acc = acc | UPPERCASE_MASK};
-                if c.is_whitespace() {acc = acc | WHITESPACE_MASK};
+                if c.is_alphabetic() {acc |= ALPHA_MASK};
+                if c.is_alphanumeric() {acc |= ALPHANUMERIC_MASK};
+                if c.is_digit(10) {acc |= DIGIT_MASK};
+                if c.is_lowercase() {acc |= LOWERCASE_MASK};
+                if c.is_uppercase() {acc |= UPPERCASE_MASK};
+                if c.is_whitespace() {acc |= WHITESPACE_MASK};
+                if c == '\n' {acc |= NEWLINE_MASK};
                 acc
             });
 
             SourceCluster{
                 grapheme_cluster: Rc::new(source),
+                column,
+                line,
                 unicode_characteristics,
             }
         }
 
+        #[inline]
         fn test_characteristic(&self, mask: u8) -> bool {
             self.unicode_characteristics & mask != 0
         }
 
+        /// getter for the line of the source cluster
+        pub fn line(&self) -> u32 {
+            self.line
+        }
+
+        /// getter for the column of the source cluster
+        pub fn column(&self) -> u32 {
+            self.column
+        }
+
+        /// getter for the underlying grapheme cluster
+        pub fn grapheme_cluster(&self) -> &str {
+            &self.grapheme_cluster
+        }
+
+        /// true if true for the first char of the owned grapheme cluster
         pub fn is_alphabetic(&self) -> bool {
             self.test_characteristic(ALPHA_MASK)
         }
 
+        /// true if true for the first char of the owned grapheme cluster
         pub fn is_alphanumeric(&self) -> bool {
             self.test_characteristic(ALPHANUMERIC_MASK)
         }
 
+        /// true if true for the first char of the owned grapheme cluster
         pub fn is_base10_digit(&self) -> bool {
             self.test_characteristic(DIGIT_MASK)
         }
 
+        /// true if true for the first char of the owned grapheme cluster
         pub fn is_lowercase(&self) -> bool {
             self.test_characteristic(LOWERCASE_MASK)
         }
 
+        /// true if true for the first char of the owned grapheme cluster
         pub fn is_uppercase(&self) -> bool {
             self.test_characteristic(UPPERCASE_MASK)
         }
 
+        /// true if true for the first char of the owned grapheme cluster
         pub fn is_whitespace(&self) -> bool {
             self.test_characteristic(WHITESPACE_MASK)
+        }
+
+        /// true if true for the first char of the owned grapheme cluster
+        pub fn is_newline(&self) -> bool {
+            self.test_characteristic(NEWLINE_MASK)
         }
     }
 
     #[cfg(test)]
     mod tests {
-        use std::error::Error;
         use super::SourceCluster;
 
         fn test_cluster(source: &str) -> SourceCluster {
-            SourceCluster::new(source.to_string())
+            SourceCluster::new(source.to_string(), 0, 0)
         }
 
         #[test]
@@ -156,7 +290,27 @@ mod source_cluster {
 
         #[test]
         fn non_whitespace_source_cluster_is_not_whitespace() {
-            assert!(!test_cluster("a").is_whitespace())
+            assert!(!test_cluster("a").is_whitespace());
+        }
+
+        #[test]
+        fn newline_is_newline() {
+            assert!(test_cluster("\n").is_newline());
+        }
+
+        #[test]
+        fn line_returns_the_line_property() {
+            assert_eq!(SourceCluster::new("a".to_string(), 1, 2).line(), 1);
+        }
+
+        #[test]
+        fn column_returns_the_column_property() {
+            assert_eq!(SourceCluster::new("a".to_string(), 1, 2).column(), 2);
+        }
+
+        #[test]
+        fn grapheme_cluster_returns_the_grapheme_cluster() {
+            assert_eq!(test_cluster("a").grapheme_cluster(), "a");
         }
     }
 
